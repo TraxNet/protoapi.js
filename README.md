32a7a9e3c6e430c303c25e0deff3649fa98fbd6b
Oscar Blasco <traxnet@gmail.com>
2012-02-01 07:54:35 +0100

    More complex queries added to unit tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ProtoAPI
 
-This library sets a thin layer on top of the ProtoAPI REST end-points to allow simple access to ProtoAPI services from javascript using jQuery for ajax calls.
+This library sets a thin layer on top of the ProtoAPI REST end-points to allow simple access to ProtoAPI services from javascript. It uses jQuery internally to perform browser-independent ajax calls.
 
 ## Adding ProtoAPI to your Unity3d game
 
--- tests/tests.js
+++ tests/tests.js
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 
 /** NOTE: Change this according to your appid and appkey */
 var config = { appid: '4f20ef0800ac0fa0a20000a2', appkey: 'f0e4bd6cb4452f953d400d488e6663ec' };
@@ -119,4 +119,215 @@ test('Simple Queries', function(){
 			start();
 		});
 	}, 3000);
+});
+
+function simpleRandString(length) {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+    
+    if (! length) {
+        length = Math.floor(Math.random() * chars.length);
+    }
+    
+    var str = '';
+    for (var i = 0; i < length; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+
+	
+
+	
+
+// Operand queries
+test('Operand Queries', function(){
+	expect(36);
+	
+	var test_id = simpleRandString(40);
+	
+	function genenerateFakePost(t_id){
+		var post = { 
+			name: simpleRandString(), 
+			user: simpleRandString(), 
+			date: (new Date()).toISOString(), 
+			type: 'op',
+			test_id: t_id
+		};
+		return post;
+	}
+
+	stop();
+
+	// Add 1000 objects to protoapi under 'posts' collection
+	setTimeout(function (){
+		ok(true, 'code runs');
+		var array = [];
+
+		for(i=0; i < 10; i++){
+			var post = genenerateFakePost( test_id );
+			array.push(i);
+			post.array = array;
+			post.num = i;
+
+			$("op_posts").protoapi(config).post(post, function( num ){
+				return function( response ){ 
+					ok(true, 'ajax success');
+					equal(typeof(response.data), 'string', 'Response data must not be undefined');
+					if( num === 9 ){
+						start();
+					}
+				}
+			}( i ));
+		}
+	}, 1000);
+
+	stop();
+
+	// Find them back
+	setTimeout(function (){
+		ok(true, 'code runs');
+
+		
+		$("op_posts").protoapi(config).get({ type: 'op', test_id: test_id }, function( response ){ 
+			ok(true, 'ajax success');
+			equal(response.count, 10, 'We expect 10 objects');
+			start();
+		});
+	}, 3000);
+
+	stop();
+
+	// $in operator
+	setTimeout(function (){
+		ok(true, 'code runs');
+
+		
+		$("op_posts").protoapi(config).get({ array: { $in: [ 7, 8, 9 ] }, test_id: test_id }, function( response ){ 
+			ok(true, 'ajax success');
+			equal(response.count, 3, 'We expect 3 objects');
+			start();
+		});
+	}, 3000);
+
+	stop();
+
+	// $nin operator
+	setTimeout(function (){
+		ok(true, 'code runs');
+
+		
+		$("op_posts").protoapi(config).get({ array: { $nin: [ 7, 8, 9 ] }, test_id: test_id }, function( response ){ 
+			ok(true, 'ajax success');
+			equal(response.count, 7, 'We expect 7 objects');
+			start();
+		});
+	}, 3000);
+
+	stop();
+
+	// $all operator
+	setTimeout(function (){
+		ok(true, 'code runs');
+
+		
+		$("op_posts").protoapi(config).get({ array: { $all: [ 3, 7, 8, 9 ] }, test_id: test_id }, function( response ){ 
+			ok(true, 'ajax success');
+			equal(response.count, 1, 'We expect 1 object');
+			start();
+		});
+	}, 3000);
+
+	stop();
+
+	// $size operator
+	setTimeout(function (){
+		ok(true, 'code runs');
+
+		
+		$("op_posts").protoapi(config).get({ array: { $size: 2 }, test_id: test_id }, function( response ){ 
+			ok(true, 'ajax success');
+			equal(response.count, 1, 'We expect 1 object');
+			start();
+		});
+	}, 3000);
+});
+
+/** Some queries with dates */
+test('Date queries', function(){
+	expect(17);
+
+	var test_id = simpleRandString(40);
+
+	var config = { appid: '4f20ef0800ac0fa0a20000a2', appkey: 'f0e4bd6cb4452f953d400d488e6663ec' };
+	var date0 = new Date(Date.UTC(2001, 1, 3, 4, 5, 6, 0));
+	var date1 = new Date(Date.UTC(2001, 1, 3, 4, 6, 6, 0));
+	var post0 = { name: 'test_dates', user: 'ralph', date: date0.toISOString(), test_id: test_id };
+	var post1 = { name: 'test_dates', user: 'ralph', date: date1.toISOString(), test_id: test_id };
+
+	stop();
+
+	setTimeout(function (){
+		ok(true, 'code runs');
+
+		$("posts").protoapi(config).post( post0, function( response ){ 
+			ok(true, 'ajax success');
+			notEqual(response.data, undefined, 'Response data must not be undefined');
+		});
+
+		$("posts").protoapi(config).post( post1, function( response ){ 
+			ok(true, 'ajax success');
+			notEqual(response.data, undefined, 'Response data must not be undefined');
+			start();
+		});
+	}, 1000);
+
+	stop();
+
+	setTimeout(function (){
+		ok(true, 'code runs');
+
+		
+		$("posts").protoapi(config).get({ name: 'test_dates', test_id: test_id }, function( response ){ 
+			ok(true, 'ajax success');
+			equal(response.data.length, 2, 'Response data contains 2 objects');
+			start();
+		});
+	}, 1000);
+
+	stop();
+
+	setTimeout(function (){
+		ok(true, 'code runs');
+
+		
+		$("posts").protoapi(config).get({ date: date0.toISOString(), test_id: test_id }, function( response ){ 
+			ok(true, 'iso date query success');
+			equal(response.data.length, 1, 'Response data contains 1 object');
+			start();
+		});
+	}, 1000);
+
+	stop();
+
+	setTimeout(function (){
+		ok(true, 'code runs');
+		
+		$("posts").protoapi(config).get({ date: { $gt: date0.toISOString() }, test_id: test_id }, function( response ){ 
+			ok(true, 'iso date get $gt success');
+			equal(response.data.length, 1, 'Response data contains 1 object');
+			start();
+		});
+	}, 1000);
+
+	stop();
+
+	setTimeout(function (){
+		ok(true, 'code runs');
+		
+		$("posts").protoapi(config).get({ date: { $lt: date1.toISOString() }, test_id: test_id }, function( response ){ 
+			ok(true, 'iso date get $lt success');
+			equal(response.data.length, 1, 'Response data contains 1 object');
+			start();
+		});
+	}, 1000);
 });
